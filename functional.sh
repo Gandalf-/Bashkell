560ff16534c6d24a248669136d5a122fdb8c8683
Gandalf- <austin.voecks@gmail.com>
2017-01-31 20:34:38 -0800

    Filter, map for two lists, boolean functions

--- functional.sh
+++ functional.sh
@@ -5,9 +5,17 @@ sub() { echo $(( $1 - $2 )); }
 mul() { echo $(( $1 * $2 )); }
 div() { echo $(( $1 / $2 )); }
 
+odd() { [[ $(( $1 % 2 )) == 1 ]]; }
+even(){ [[ $(( $1 % 2 )) == 0 ]]; }
+not() { eval "$@"; if ! (($?)); then false; fi; }
+empty(){ [[ -z "$@" ]]; }
+
 cons(){ echo "$@"; }
 cdr() { shift; echo "$@"; }
 car() { echo "$1"; }
+
+cadr() { . car . cdr "$@"; }
+cddr() { . cdr . cdr "$@"; }
 
 length() { echo "${#@}"; }
 append() { echo "${@:2} ${1}"; }
@@ -16,9 +24,10 @@ reverse(){ for ((i=$#; i >= 1; i--)); do echo "${!i}"; done | xargs ; }
 list_tail(){ echo ${@:$(( $1 + 2)) }; }
 list_ref() { echo ${@:$(( $1 + 2)):1 }; }
 
-curry() {
+.() {
   : ' function, ... -> any
   evaluates an arbitrary number of curried functions from right to left
+  . mul 5 . add 5 . sub 10 5 -> 50
   '
   IFS='.' read -ra args <<< "$@"
 
@@ -33,18 +42,40 @@ curry() {
 }
 
 map() {
-  : ' function, list -> list
+  : ' function, list [, list] -> list
   applies the provided function to every element of the list
+  map add 5 , {1..5} -> 6 7 8 9 10
+  map add , {1..5} , {6..10} -> 7 9 11 13 15
   '
+  local func list1 list2 args
+
   IFS=',' read -ra args <<< "$@"
-  for arg in ${args[@]:1}; do
-    eval "${args[0]} $arg"
-  done | xargs
+  func="${args[0]}"
+  list1="${args[1]}"
+  list2="${args[2]}"
+
+  # single list
+  if [[ -z "$list2" ]]; then
+
+    for elem in $list1; do
+      eval "$func $elem"
+    done | xargs
+
+  # two lists
+  else
+
+    while not empty "$list1" && not empty "$list2"; do
+      eval "$func $(car $list1) $(car $list2)"
+      list1="$(cdr $list1)"
+      list2="$(cdr $list2)"
+    done | xargs
+  fi
 }
 
 map_lambda() {
   : ' anonymous function, list -> list
   more powerful version of map that allows arbitrary functions to be defined
+  possibly depreciated
   '
   IFS=',' read -ra args <<< "$@"
   eval "function _lambda() { "${args[0]}" ;}"
@@ -56,46 +87,68 @@ foldl() {
   applies the provided function to each element of the list, instead of
   returning a list, the input is applied to init in a way defined by the
   provided function
+  foldl add , 100 , {1..5}
   '
   IFS=',' read -ra args <<< "$@"
-  func="${args[0]}"
-  init="${args[@]:1:1}"
+  func="$(car "${args[@]}")"
+  init="$(cadr "${args[@]}")"
+  list="$(cddr "${args[@]}")"
 
-  for arg in ${args[@]:2}; do
-    init=$(eval "$func $init $arg")
-  done 
+  for elem in $list; do
+    init="$(eval "$func $init $elem ")"
+  done
 
   echo $init
 }
 
-curry reverse . cdr . reverse {a..f}
-echo
+filter() {
+  : ' function, list -> list
+  applies the function to every element in the list, if the return value is
+  true then that element is added to the output list
+  '
+  IFS=',' read -ra args <<< "$@"
+  func="$(car "${args[@]}")"
+  list="$(cdr "${args[@]}")"
 
-curry car . cdr . cdr {a..e}
-echo
+  for elem in $list; do
+    if $(eval "$func" "$elem"); then
+      echo "$elem"
+    fi
+  done | xargs
+}
 
-map 'echo -n', {a..e}
-echo
+examples() {
+  . reverse . cdr . reverse {a..f}
 
-curry echo . cdr . map echo, {a..f}
-echo
+  . car . cdr . cdr {a..e}
 
-curry cons foo . map mul 5 , . cdr {1..5}
-echo
+  map 'echo -n', {a..e}
 
-map 'rot13 <<<', Hello there foo
-echo
+  . echo . cdr . map echo, {a..f}
 
-map_lambda 'echo $(($1 + 5))', {1..6}
-echo
+  . cons foo . map mul 5 , . cdr {1..5}
 
-map_lambda 'rot13 <<< $1 | rot13', Hello there
-echo
+  map 'rot13 <<<', Hello there foo
 
-curry mul 5 . add 1 . add 2 3
-echo
+  map_lambda 'echo $(($1 + 5))', {1..6}
 
-curry echo a. echo b. echo c
-echo
+  map_lambda 'rot13 <<< $1 | rot13', Hello there
 
-foldl add , 10 , {1..10}
+  . mul 5 . add 1 . add 2 3
+
+  . echo a. echo b. echo c
+
+  foldl add , 10 , {1..10}
+
+  . map echo , . reverse {a..f}
+}
+
+while true; do
+  echo -n "> "; read -r line
+
+  if [[ "$line" == "quit" ]]; then
+    exit
+  fi
+
+  eval "$line"
+done
