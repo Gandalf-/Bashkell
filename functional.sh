d14bbe2efd02a2165a99c6820cb3f1124f251388
Gandalf- <austin.voecks@gmail.com>
2017-02-05 10:35:38 -0800

    Performance improvements, add flip

--- functional.sh
+++ functional.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-add() { echo $(( $1 + $2 )); }
-sub() { echo $(( $1 - $2 )); }
-mul() { echo $(( $1 * $2 )); }
-div() { echo $(( $1 / $2 )); }
+# usage: 
+#  source functional.sh
+#  echo "done!"
+
+tmp_fifo='/tmp/funcfifo'
+rm -f ${tmp_fifo}
+mkfifo ${tmp_fifo}
+exec 5<>${tmp_fifo}
+
+# core
+add() { 
+  if int "$1" && int "$2"; then echo $(( $1 + $2 ))
+  else bc <<< "scale=4; $1 + $2"; fi
+}
+sub() { 
+  if int "$1" && int "$2"; then echo $(( $1 - $2 ))
+  else bc <<< "scale=4; $1 - $2"; fi
+}
+mul() { 
+  if int "$1" && int "$2"; then echo $(( $1 * $2 ))
+  else bc <<< "scale=4; $1 * $2"; fi
+}
+div() {
+  if int "$1" && int "$2"; then echo $(( $1 / $2 ))
+  else bc <<< "scale=4; $1 / $2"; fi
+}
+mod() { 
+  if int "$1" && int "$2"; then echo $(( $2 % $1 ))
+  else bc <<< "scale=4; $2 % $1"; fi
+}
 
 odd() { [[ $(( $1 % 2 )) == 1 ]]; }
 even(){ [[ $(( $1 % 2 )) == 0 ]]; }
 not() { eval "$@"; if ! (($?)); then false; fi; }
 empty(){ [[ -z "$@" ]]; }
+int() { [[ "$1" -eq "$1" ]] 2>/dev/null; }
 
 cons(){ echo "$@"; }
 cdr() { shift; echo "$@"; }
@@ -21,8 +48,30 @@ length() { echo "${#@}"; }
 append() { echo "${@:2} ${1}"; }
 reverse(){ for ((i=$#; i >= 1; i--)); do echo "${!i}"; done | xargs ; }
 
-list_tail(){ echo ${@:$(( $1 + 2)) }; }
+drop(){ echo ${@:2:$(($# - $1 - 1))}; }
+take(){ echo ${@:2:$1}; }
+last() { . car . reverse "$@"; }
+
+list_tail(){ n=$1; shift; echo ${@:(-$n)}; }
 list_ref() { echo ${@:$(( $1 + 2)):1 }; }
+
+where() {
+  : ' variable = expression
+  this method of variable assignment is 6 times faster than subshells
+
+  where foo = car {1..10}
+  where foo = car {1..10} , bar = cdr {5..15}
+  '
+  local var="$1"; shift ; shift
+  "$@" >&5
+  read ${var} <&5
+}
+
+assign() {
+  local var="$1"; shift
+  "$@" >&5
+  read ${var} <&5
+}
 
 .() {
   : ' function, ... -> any
@@ -65,21 +114,13 @@ map() {
   else
 
     while not empty "$list1" && not empty "$list2"; do
-      eval "$func $(car $list1) $(car $list2)"
-      list1="$(cdr $list1)"
-      list2="$(cdr $list2)"
+      where arg1 = car $list1
+      where arg2 = car $list2
+      eval "$func $arg1 $arg2"
+      where list1 = cdr $list1
+      where list2 = cdr $list2
     done | xargs
   fi
-}
-
-map_lambda() {
-  : ' anonymous function, list -> list
-  more powerful version of map that allows arbitrary functions to be defined
-  possibly depreciated
-  '
-  IFS=',' read -ra args <<< "$@"
-  eval "function _lambda() { "${args[0]}" ;}"
-  map _lambda, "${args[@]:1}" | xargs
 }
 
 foldl() {
@@ -89,13 +130,15 @@ foldl() {
   provided function
   foldl add , 100 , {1..5}
   '
+  local func init list args
+
   IFS=',' read -ra args <<< "$@"
-  func="$(car "${args[@]}")"
-  init="$(cadr "${args[@]}")"
-  list="$(cddr "${args[@]}")"
+  func="${args[0]}"
+  init="${args[1]}"
+  list="${args[2]}"
 
   for elem in $list; do
-    init="$(eval "$func $init $elem ")"
+    where init = eval $func $init $elem
   done
 
   echo $init
@@ -105,10 +148,11 @@ filter() {
   : ' function, list -> list
   applies the function to every element in the list, if the return value is
   true then that element is added to the output list
+  filter even , {1..100}
   '
   IFS=',' read -ra args <<< "$@"
-  func="$(car "${args[@]}")"
-  list="$(cdr "${args[@]}")"
+  where func = car "${args[@]}"
+  where list = cdr "${args[@]}"
 
   for elem in $list; do
     if $(eval "$func" "$elem"); then
@@ -117,38 +161,41 @@ filter() {
   done | xargs
 }
 
-examples() {
-  . reverse . cdr . reverse {a..f}
-
-  . car . cdr . cdr {a..e}
-
-  map 'echo -n', {a..e}
-
-  . echo . cdr . map echo, {a..f}
-
-  . cons foo . map mul 5 , . cdr {1..5}
-
-  map 'rot13 <<<', Hello there foo
-
-  map_lambda 'echo $(($1 + 5))', {1..6}
-
-  map_lambda 'rot13 <<< $1 | rot13', Hello there
-
-  . mul 5 . add 1 . add 2 3
-
-  . echo a. echo b. echo c
-
-  foldl add , 10 , {1..10}
-
-  . map echo , . reverse {a..f}
+lambda() {
+  eval "function _lambda() { eval "$@"; }"
+  export -f _lambda
+  echo _lambda
 }
 
-while true; do
-  echo -n "> "; read -r line
+flip() {
+  : ' function x y -> function y x
+  flip sub , 10 , 5
+  '
+  IFS=',' read -ra args <<< "$@"
+  func="${args[0]}"
+  arg1="${args[1]}"
+  arg2="${args[2]}"
 
-  if [[ "$line" == "quit" ]]; then
-    exit
-  fi
+  eval "$func $arg2 $arg1" | xargs
+}
 
-  eval "$line"
-done
+sum() { foldl add, 0, "$@"; }
+avg() { . div $(sum "$@") . length "$@"; }
+srt() { printf '%s\n' "$@" | sort -n; }
+rand() { for ((i=0; i < $1; i++)); do echo $RANDOM; done | xargs; }
+
+examples() {
+  . reverse . cdr . reverse {a..f}
+  . car . cdr . cdr {a..e}
+  map 'echo -n', {a..e}
+  . echo . cdr . map echo, {a..f}
+  . cons foo . map mul 5 , . cdr {1..5}
+  map 'rot13 <<<', Hello there foo
+  . mul 5 . add 1 . add 2 3
+  . echo a. echo b. echo c
+  foldl add , 10 , {1..10}
+  . map echo , . reverse {a..f}
+  drop 3 {1..10}
+  . reverse . list_tail 5 . take 50 {1..100}
+  . filter not even , . map mod 5 , {1..100}
+}
